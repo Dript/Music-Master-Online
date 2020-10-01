9dbf8b68bcd28f61d48f631b8c41b6102c1e8508
ShubhamSingh2006 <72137926+ShubhamSingh2006@users.noreply.github.com>
2020-10-01 09:04:53 +0530

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -5,6 +5,11 @@ app_name = 'music'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^logout_user/$', views.logout_user, name='logout_user'),
+    url(r'^create_album/$', views.create_album, name='create_album'),
+    url(r'^(?P<album_id>[0-9]+)/create_song/$', views.create_song, name='create_song'),
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<song_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
     url(r'^songs/(?P<filter_by>[a-zA_Z]+)/$', views.songs, name='songs'),   
